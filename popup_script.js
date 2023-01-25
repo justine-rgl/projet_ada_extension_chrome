2493e7f70fa87fbf4db73b041329800583b2f8e6
justine-rgl <justine.rougeulle@gmail.com>
2023-01-25 17:41:21 +0100

    Project version finale

--- popup_script.js
+++ popup_script.js
@@ -1,14 +1,49 @@
 //on attend que la page soit fully loadée pour faire des trucs
 document.addEventListener('DOMContentLoaded', function (){  
     //on écoute le bouton pour créer l'objet
-    document.querySelector('.increase').addEventListener('click', onclick, false)
-    function onclick(){
+    document.querySelector('.increase').addEventListener('click', onclickIncrease, false)
+    function onclickIncrease(){
         chrome.storage.local.set({increase: true})
     };
 
-    // document.querySelector('.decrease').addEventListener('click', onclick, false)
-    // function onclick(){
-    //     chrome.storage.local.set({decrease: true})
-    // };
+    document.querySelector('.decrease').addEventListener('click', onclickDecrease, false)
+    function onclickDecrease(){
+        chrome.storage.local.set({decrease: true})
+    };
 
+    const toggle = document.querySelector(".toggle");
+    toggle.addEventListener("click", onclickToggle, false)
+    chrome.storage.local.set({toggle: false})
+    function onclickToggle() {
+    chrome.storage.local.get(["toggle"]).then((result) => {
+        if(result.toggle == false) {
+            console.log("COUCOU IF")
+            function appOn() {
+                document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
+                let media = document.querySelectorAll("img, picture, video");
+                media.forEach((mediaItem) => {
+                mediaItem.style.filter = "invert(1) hue-rotate(180deg)";
+                })
+            }
+
+            appOn()
+            chrome.storage.local.set({toggle: true})
+        }
+
+        else {
+            console.log("COUCOU ELSE")
+             function appOff() {
+                document.querySelector("html").style.filter = "invert(0) hue-rotate(0deg)";
+                let media = document.querySelectorAll("img, picture, video");
+                media.forEach((mediaItem) => {
+                mediaItem.style.filter = "invert(0) hue-rotate(0deg)";
+                })
+            }
+
+            appOff()
+            chrome.storage.local.set({toggle: false})
+
+        }
+    })
+}
 }, false);
--- script.js
+++ script.js
@@ -1,27 +1,56 @@
 //on écoute le changement du storage déclenché par le bouton de la popup
 chrome.storage.local.onChanged.addListener((changes, local) => {
-    // chrome.storage.local.get('decrease').then((result) => {
-    //     if (result.decrease == true) {
-    //         console.log("result.decrease")
-    //         decreaseTextSize()
-    //         chrome.storage.local.set({'decrease': false})  
-    //     }
-    // })
+    chrome.storage.local.get('decrease').then((result) => {
+        if (result.decrease == true) {
+            console.log("result.decrease")
+            decreaseTextSize()
+            chrome.storage.local.set({'decrease': false})  
+        }
+    })
     chrome.storage.local.get('increase').then((result) => {
         if (result.increase == true) {
-            console.log("result.increase")
             increaseTextSize()
             chrome.storage.local.set({'increase': false})  
         }
     })
 })
 
-// function decreaseTextSize() {
-//     let texts = document.querySelectorAll("h1, h2, h3, h4, h5, h6, txt, text, p, a, li, td");
-//     for (let i=0 ; i<texts.length ; i++) {
-//         texts[i].style.fontSize = "5px";
-//     }
-// }
+chrome.storage.local.onChanged.addListener((changes, local) => {
+    chrome.storage.local.get(["toggle"]).then((result) => {
+        if (result.toggle == true) {
+        function appOn() {
+            document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
+        
+            let media = document.querySelectorAll("img, picture, video");
+        
+            media.forEach((mediaItem) => {
+            mediaItem.style.filter = "invert(1) hue-rotate(180deg)";
+            })
+        }
+
+        appOn()
+        } else {
+        function appOff() {
+            document.querySelector("html").style.filter = "invert(0) hue-rotate(0deg)";
+        
+            let media = document.querySelectorAll("img, picture, video");
+        
+            media.forEach((mediaItem) => {
+            mediaItem.style.filter = "invert(0) hue-rotate(0deg)";
+            })
+        }
+
+        appOff()
+        }
+    });
+})
+
+function decreaseTextSize() {
+    let texts = document.querySelectorAll("h1, h2, h3, h4, h5, h6, txt, text, p, a, li, td");
+    for (let i=0 ; i<texts.length ; i++) {
+        texts[i].style.fontSize = "10px";
+    }
+}
 
 function increaseTextSize() {
     let texts = document.querySelectorAll("h1, h2, h3, h4, h5, h6, txt, text, p, a, li, td");
